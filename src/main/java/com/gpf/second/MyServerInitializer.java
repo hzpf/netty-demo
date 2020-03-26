ba8b6c36c647c181c627497978def5d295b31001
ge.pengfei <953952913@qq.com>
2020-03-26 22:38:11 +0800

    cmmm

---
+++ src/main/java/com/gpf/second/MyServerInitializer.java
@@ -0,0 +1,18 @@
+package com.gpf.second;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+public class MyServerInitializer extends ChannelInitializer {
+    @Override
+    protected void initChannel(Channel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
