01adfbab3451f194777322eec01fb27ef609a8b2
ge.pengfei <953952913@qq.com>
2020-03-26 16:44:36 +0800

    cmmm

---
+++ src/main/java/com/gpf/first/TestServerInitializer.java
@@ -0,0 +1,15 @@
+package com.gpf.first;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer {
+    @Override
+    protected void initChannel(Channel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new TestHttpServerHandler());
+    }
+}
